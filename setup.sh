97a7d9b204726570101173169928f6ba59cc6739
Josh Sherman <josh@gravityblvd.com>
2012-09-18 14:55:27 -0400

    Abstracted vim logic into a separate script
    Added Flex plugin for vim as well

--- setup.sh
+++ setup.sh
@@ -123,37 +123,6 @@ fi" >> ~/.bashrc
 	ln -s $PWD/nautilus-scripts ~/.gnome2/nautilus-scripts
 fi
 
-# Copies .vimrc
-if [ -f ~/.vimrc ];
-then
-	rm ~/.vimrc
-fi
-
-ln -s $PWD/vimrc ~/.vimrc
-
-# Pulls down and installs vim plugins
-DOTVIM=~/.vim
-
-if [ -d $DOTVIM ];
-then
-	rm -r $DOTVIM
-fi
-
-mkdir $DOTVIM
-
-OWNERS=( "ervandew" "msanders"     "nvie"            "scrooloose" "scrooloose" "tpope"        "vim-scripts" )
-REPOS=(  "supertab" "snipmate.vim" "vim-togglemouse" "nerdtree"   "syntastic"  "vim-fugitive" "Command-T"   )
-
-for (( i = 0 ; i < ${#OWNERS[@]} ; i++ ))
-do
-	git clone git://github.com/${OWNERS[$i]}/${REPOS[$i]}.git /tmp/${REPOS[$i]}
-	cp -R /tmp/${REPOS[$i]}/* $DOTVIM
-	rm -rf /tmp/${REPOS[$i]}
-done
-
-# Finish up Command-T installation
-cd $DOTVIM/ruby/command-t
-ruby extconf.rb
-make
+./vim.sh
 
 echo; echo "SETUP COMPLETE!!~!"
