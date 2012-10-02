9cccff87b0e74315d0fe8b1b8f68c38fb6e984e6
Josh Sherman <josh@gravityblvd.com>
2012-10-02 13:36:12 +0000

    Reworked server install a bit

--- setup.sh
+++ setup.sh
@@ -9,10 +9,11 @@ then
 fi
 
 ln -s $PWD/gitconfig ~/.gitconfig
-ln -s ~/Dropbox/Pictures/Git\ Shots ~/.gitshots
 
 if [ `uname` == 'Darwin' ];
 then
+	ln -s ~/Dropbox/Pictures/Git\ Shots ~/.gitshots
+
 	# Installs Homebrew
 	if [ `which brew` == '' ];
 	then
@@ -96,38 +97,24 @@ then
 fi
 " >> ~/.bash_profile
 
+	# Installs my git hooks
+	sudo ln -s $PWD/git/hooks/post-commit /usr/local/share/git-core/templates/hooks/post-commit
 else
-	# TODO Add prompts so I can run pieces of the set up
+	# Gives us access to `add-apt-repository`
+	sudo apt-get install python-software-properties
 
-	# Adds some third party repos
-	sudo add-apt-repository ppa:tiheum/equinox          # Faenza Icons
-	sudo add-apt-repository ppa:tualatrix/ppa           # Ubuntu Tweak
-	sudo add-apt-repository ppa:webupd8team/gnome3      # WebUpd8 Gnome Extensions
-	sudo add-apt-repository ppa:otto-kesselgulasch/gimp # Gimp 2.8
+	# Adds the PostgreSQL repository so we can install 9.2
+	sudo add-apt-repository ppa:pitti/postgresql
 
 	# Updates the package list
 	sudo apt-get update
 
-	# Gets rid of some bullshit packages
-	#sudo apt-get --purge autoremove appmenu-gtk appmenu-gtk3 zeitgeist gwibber gnome-screensaver banshee
+	# Purges PostgreSQL 9.1 just in case
+	# Seems this tries to remove 9.2 as well, leaving in just in case I need to run it
+	# sudo apt-get --purge remove postgresql postgresql-9.1 postgresql-client-9.1 postgresql-client-common postgresql-common postgresql-doc postgresql-doc-9.1
 
 	# Installs CLI apps
-	sudo apt-get install vim ssh multitail htop iotop
-
-	# Installs desktop environment
-	sudo apt-get install gnome-shell gnome-tweak-tool faenza-icon-theme network-manager-openconnect-gnome gnome-sushi gnome-shell-extensions-mediaplayer gnome-shell-extensions-noa11y gnome-shell-classic-systray gnome-shell-message-notifier gnome-shell-extension-notesearch
-
-	# Installs non-CLI apps
-	sudo apt-get install gnome-agave gimp inkscape tomboy rhythmbox chromium-browser shutter ubuntu-tweak vim-gnome
-
-	# Installs my dev stack
-	sudo apt-get install git-core ruby1.8-dev nginx apache2 php5 php5-cgi php5-cli php-pear php5-suhosin psmisc spawn-fcgi mysql-server php5-mysql redis-server memcached php5-memcache php5-memcached php5-gd php5-curl php5-imagick exim4-daemon-light
-
-	# TODO Installs my server stack
-
-	# Configures some stuff
-	sudo a2enmod expires rewrite ssl
-	sudo service apache2 restart
+	sudo apt-get install vim ssh multitail htop iotop git-core ruby1.8-dev nginx php5 php5-cgi php5-cli php-pear php5-suhosin php5-fpm redis-server memcached php5-memcache php5-memcached php5-gd php5-curl php5-imagick php5-pgsql exim4-daemon-light postgresql-9.2
 
 	# Clears out the old .bashrc
 	if [ -f ~/.bashrc ];
@@ -145,19 +132,7 @@ export PATH=\"$PWD/git/scripts:$PATH\"
 if [ -f $PWD/bashrc ] && ! shopt -oq posix; then
 	. $PWD/bashrc
 fi" >> ~/.bashrc
-
-	# Removes the existing scripts
-	if [ -d ~/.gnome2/nautilus-scripts ];
-	then
-		rm ~/.gnome2/nautilus-scripts -rf
-	fi
-
-	# Symlinks back to our scripts
-	ln -s $PWD/nautilus-scripts ~/.gnome2/nautilus-scripts
 fi
-
-# Installs my git hooks
-sudo ln -s $PWD/git/hooks/post-commit /usr/local/share/git-core/templates/hooks/post-commit
 
 ./vim.sh
 
