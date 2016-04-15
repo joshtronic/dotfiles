fa11badc7efa87d74fe9dda6b761ff82232795fd
Josh Sherman <josh@gravityblvd.com>
2016-04-15 10:52:48 -0400

    Hammerspoon hotkeys

---
+++ hammerspoon/init.lua
@@ -0,0 +1,12 @@
+hs.hotkey.bind({"cmd", "ctrl"}, "1", function()
+  hs.application.launchOrFocus("Terminal")
+end)
+
+hs.hotkey.bind({"cmd", "ctrl"}, "2", function()
+  hs.application.launchOrFocus("Google Chrome")
+end)
+
+hs.hotkey.bind({"cmd", "ctrl"}, "3", function()
+  hs.application.launchOrFocus("Slack")
+end)
+
