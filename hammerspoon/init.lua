7067918ec2180eb88b9214d97fe421a9e9156ddc
Josh Sherman <josh@gravityblvd.com>
2016-05-25 10:53:52 -0500

    Swapped hammerspoon hotkeys with karabiner stuff

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,12 +1,1 @@
-hs.hotkey.bind({"cmd", "ctrl"}, "1", function()
-  hs.application.launchOrFocus("Terminal")
-end)
-
-hs.hotkey.bind({"cmd", "ctrl"}, "2", function()
-  hs.application.launchOrFocus("Google Chrome")
-end)
-
-hs.hotkey.bind({"cmd", "ctrl"}, "3", function()
-  hs.application.launchOrFocus("Slack")
-end)
 
