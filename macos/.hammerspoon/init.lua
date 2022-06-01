00dad3db06d9f6a48a3761447d135ce66955b271
Josh Sherman <joshsherman@gmail.com>
2022-05-31 21:59:46 -0500

    Window snapping

--- macos/.hammerspoon/init.lua
+++ macos/.hammerspoon/init.lua
@@ -1,1 +1,60 @@
--- TODO: Make it hot.
+-- Window 50% left
+hs.hotkey.bind({"cmd"}, "Left", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+
+  win:setFrame(f, 0)
+end)
+
+-- Window 50% right
+hs.hotkey.bind({"cmd"}, "Right", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x + (max.w / 2)
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+
+  win:setFrame(f, 0)
+end)
+
+-- Window 100%
+hs.hotkey.bind({"cmd"}, "Up", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w
+  f.h = max.h
+
+  win:setFrame(f, 0)
+end)
+
+-- Window "Reset"
+hs.hotkey.bind({"cmd"}, "Down", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h / 2
+
+  win:setFrame(f, 0)
+  win:centerOnScreen(screen, true, 0)
+end)
