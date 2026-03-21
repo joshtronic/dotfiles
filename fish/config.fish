cd915de280bc5fa46b58b5550e0a772af52428f1
Josh Sherman <joshsherman@gmail.com>
2026-03-21 12:52:20 -0500

    Start moving stuff from zsh to fish
    Also dropped the variables file that gets generated on the fly and has
    machine-specific bits in it.

--- fish/config.fish
+++ fish/config.fish
@@ -1,22 +1,26 @@
 #!/usr/bin/env fish
 
-### # Speed up `brew install`
-### set -gx HOMEBREW_NO_AUTO_UPDATE 1
-### set -gx HOMEBREW_NO_INSTALL_CLEANUP 1
-###
-### source $HOME/.env
-### source $HOME/.aliases
-###
-### eval (dircolors $HOME/.dircolors)
-###
-### # Load up and configure fzf, nvm, and zsh plugins
-### if test (uname) = "Darwin"
-###   # macOs
-###
-### elif type -q apt
-###   # Debian
-###
-### elif type -q pacman
-###   # Arch
-###
-### end
+# Disable the welcome message
+set fish_greeting
+
+# Setup some basic environment variables
+set -gx EDITOR vim
+set -gx QUOTING_STYLE literal
+
+# Start configuring our path
+fish_add_path $HOME/.local/bin
+
+if test (uname) = Darwin
+  fish_add_path /opt/homebrew/opt/fnm/bin
+  # Speed up `brew install`
+  set -gx HOMEBREW_NO_AUTO_UPDATE 1
+  set -gx HOMEBREW_NO_INSTALL_CLEANUP 1
+else
+  fish_add_path $HOME/.local/share/fnm
+  set -gx SSH_AUTH_SOCK $XDG_RUNTIME_DIR/gcr/ssh
+end
+
+# Wire up fnm if it's available
+if type -q fnm
+  fnm env --shell fish --use-on-cd --version-file-strategy recursive | source
+end
