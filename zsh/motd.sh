0125b83717b55db20b05b1a8091ad19326a3cb1e
Josh Sherman <joshsherman@gmail.com>
2026-04-04 21:32:12 -0500

    Less flash on that MOTD

--- zsh/motd.sh
+++ zsh/motd.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
-if command -v cbonsai &> /dev/null && command -v fortune &> /dev/null; then
-  cbonsai -m "$(fortune -s)" -p
+if command -v toilet &> /dev/null; then
+  toilet -tf ascii12 --filter metal "WELCOME"
+fi
+
+if command -v fortune &> /dev/null; then
+  fortune
 fi
 
 exec zsh
