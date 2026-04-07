b5f351316f92889955fb85974de798dbc7478718
Josh Sherman <joshsherman@gmail.com>
2026-04-06 22:34:51 -0500

    Fbterm colors, dump right into fbterm / tmux

--- zsh/tty.zsh
+++ zsh/tty.zsh
@@ -1,23 +1,23 @@
 TTY_NAME="$(tty | sed 's|/dev/||')"
 
-if [[ "$TTY_NAME" =~ ^tty[0-9]+$ ]]; then
+if [[ "$(uname)" == Linux ]]; then
   # Rose Pine Moon palette for the bare Linux TTY
-  printf '\e]P0232136' # background / black
-  printf '\e]P1eb6f92' # red
-  printf '\e]P23e8fb0' # green
-  printf '\e]P3f6c177' # yellow
-  printf '\e]P49ccfd8' # blue
-  printf '\e]P5c4a7e7' # magenta
-  printf '\e]P6ea9a97' # cyan
-  printf '\e]P7e0def4' # foreground / white
-  printf '\e]P86e6a86' # bright black
-  printf '\e]P9eb6f92' # bright red
-  printf '\e]PA3e8fb0' # bright green
-  printf '\e]PBf6c177' # bright yellow
-  printf '\e]PC9ccfd8' # bright blue
-  printf '\e]PDc4a7e7' # bright magenta
-  printf '\e]PEea9a97' # bright cyan
-  printf '\e]PFe0def4' # bright white
+  printf '\e]P0232136'
+  printf '\e]P1eb6f92'
+  printf '\e]P23e8fb0'
+  printf '\e]P3f6c177'
+  printf '\e]P49ccfd8'
+  printf '\e]P5c4a7e7'
+  printf '\e]P6ea9a97'
+  printf '\e]P7e0def4'
+  printf '\e]P86e6a86'
+  printf '\e]P9eb6f92'
+  printf '\e]PA3e8fb0'
+  printf '\e]PBf6c177'
+  printf '\e]PC9ccfd8'
+  printf '\e]PDc4a7e7'
+  printf '\e]PEea9a97'
+  printf '\e]PFe0def4'
 
   # Rose Pine Moon palette for fbterm
   echo -en '\e[3;0;35;33;54}'
@@ -38,14 +38,20 @@ if [[ "$TTY_NAME" =~ ^tty[0-9]+$ ]]; then
   echo -en '\e[3;15;224;222;244}'
 
   clear
+fi
 
-  # if [[ -z "$TMUX" ]]; then
-  #   SESSION="${TTY_NAME:-tty}"
-  #
-  #   if tmux has-session -t "$SESSION" 2>/dev/null; then
-  #     exec tmux attach-session -t "$SESSION"
-  #   else
-  #     exec tmux new-session -s "$SESSION" "$DOTFILES/zsh/motd.sh"
-  #   fi
-  # fi
+# Bare TTY: launch fbterm
+if [[ "$TTY_NAME" =~ ^tty[0-9]+$ ]]; then
+  exec fbterm
+fi
+
+# Inside fbterm (or any pts): launch tmux
+if [[ -z "$TMUX" ]] && [[ "$TTY_NAME" =~ ^pts/ ]]; then
+  SESSION="${TTY_NAME//\//-}"
+
+  if tmux has-session -t "$SESSION" 2>/dev/null; then
+    exec tmux attach-session -t "$SESSION"
+  else
+    exec tmux new-session -s "$SESSION" "$DOTFILES/zsh/motd.sh"
+  fi
 fi
